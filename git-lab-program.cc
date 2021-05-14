34b5369ab0637d9f41ae961ecf3458d70e403ba6
Akhil Sai Ramini <ramini.akhilsai@gmail.com>
2021-05-13 21:35:23 -0400

    First program commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Akhil Sai Ramini>
+ *        Date: 5/13/2021>
+ * Description: Add Description
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
